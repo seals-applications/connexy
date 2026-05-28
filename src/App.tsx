82a5d79cb7737b229fae6693af99267a5861df68
seals-applications <okatetsu0315@gmail.com>
2026-05-29 05:25:09 +0900

    Fix router for static hosting (use HashRouter)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { BottomNav } from './components/BottomNav';
 import { SearchPage } from './pages/SearchPage';
 import { MessagePage } from './pages/MessagePage';
